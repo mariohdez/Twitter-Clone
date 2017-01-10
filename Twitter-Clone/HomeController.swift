fb73272aff5d2bbb46f1e7e7031eec308db53891
Mario Hernandez <mhmario963@gmail.com>
2017-01-10 15:28:34 -0700

    Added a header and a footer... Just a prototype

--- Twitter-Clone/HomeController.swift
+++ Twitter-Clone/HomeController.swift
@@ -26,7 +26,7 @@ class wordCell: UICollectionViewCell {
         
     }
     func setUpView() {
-        backgroundColor = .yellow
+        backgroundColor = .white
         self.addSubview(wordLabel)
         constrainWordLabel()
         
@@ -46,17 +46,26 @@ class wordCell: UICollectionViewCell {
 }
 
 class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
+    
     let cellID = "cellID"
+    let headerID = "headerID"
+    let footerID = "footerID"
+    
     let dataForCells = ["Hello", "my", "name", "is","Mario","Hernandez"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        collectionView?.backgroundColor = UIColor.blue
+        collectionView?.backgroundColor = UIColor.green
         collectionView?.register(wordCell.self, forCellWithReuseIdentifier: cellID)
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionHeader, withReuseIdentifier: headerID)
+        collectionView?.register(UICollectionViewCell.self, forSupplementaryViewOfKind: UICollectionElementKindSectionFooter, withReuseIdentifier: footerID)
+        
     }
+    
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return dataForCells.count
     }
+    
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let curCell = collectionView.dequeueReusableCell(withReuseIdentifier: cellID, for: indexPath)
@@ -68,8 +77,33 @@ class HomeController: UICollectionViewController, UICollectionViewDelegateFlowLa
     
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
         return CGSize(width: view.frame.width, height: 50)
     }
+    
+    override func collectionView(_ collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, at indexPath: IndexPath) -> UICollectionReusableView {
+        if kind == UICollectionElementKindSectionHeader {
+            let header = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: headerID, for: indexPath)
+            header.backgroundColor = .blue
+            return header
+            
+        } else {
+            let footer = collectionView.dequeueReusableSupplementaryView(ofKind: kind, withReuseIdentifier: footerID, for: indexPath)
+            footer.backgroundColor = .red
+            return footer
+        }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForHeaderInSection section: Int) -> CGSize {
+        
+        return CGSize(width: self.view.frame.width, height: 50)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, referenceSizeForFooterInSection section: Int) -> CGSize {
+        return CGSize(width: self.view.frame.width, height: 400)
+    }
+    
     
     
 }
